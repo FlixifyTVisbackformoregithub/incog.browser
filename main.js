8fdb4a21c37a2432cf3701aa98adc5bd3ba10923
Flix <flixgit@outlook.com>
2024-10-18 16:05:32 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: __dirname + '/renderer.js'
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
